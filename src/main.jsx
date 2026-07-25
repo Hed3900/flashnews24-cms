bb43289761c6d2f3d5fac809db54f623c55b9730
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 02:55:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "white" }}>Main.jsx Working</h1>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
