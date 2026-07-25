59778623351ffd85a2405f6029744bcc54b243ef
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 00:56:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/flashnews24-cms/",
+
   plugins: [react()],
+
   server: {
     host: true,
-    port: 5173
+    port: 5173,
   },
+
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
