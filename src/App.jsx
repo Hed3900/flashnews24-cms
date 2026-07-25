ba3ac68906b99538d1eac6321902f8737a3b0819
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 00:49:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Posts from "./pages/Posts";
+import Authors from "./pages/Authors";
+import Categories from "./pages/Categories";
+import Settings from "./pages/Settings";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
