490e4fea6de655538fbcff034441c1607cf9aa7a
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 02:53:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-    </Routes>
-  );
+  return <Dashboard />;
 }
 
 export default App;
