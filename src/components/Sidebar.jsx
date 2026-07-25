722c9dca07e905d181766ac0c226645f9e44b053
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 03:31:55 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 function Sidebar() {
   return (
-    <aside style={{
-      width: "250px",
-      background: "#1e293b",
-      color: "white",
-      padding: "20px"
-    }}>
-      Sidebar Working
+    <aside className="sidebar">
+      <div className="logo">
+        FlashNews24 CMS
+      </div>
+
+      <nav>
+        <NavLink to="/">Dashboard</NavLink><br />
+        <NavLink to="/posts">Posts</NavLink><br />
+        <NavLink to="/authors">Authors</NavLink><br />
+        <NavLink to="/categories">Categories</NavLink><br />
+        <NavLink to="/settings">Settings</NavLink>
+      </nav>
     </aside>
   );
 }
