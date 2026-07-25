47ec15966dd54b631aba3929f56a50b910b49117
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 03:25:09 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
+import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 function Layout({ children }) {
   return (
-    <>
-      <Header />
-      {children}
-    </>
+    <div className="container">
+      <Sidebar />
+      <div className="main">
+        <Header />
+        <main className="content">
+          {children}
+        </main>
+      </div>
+    </div>
   );
 }
 
