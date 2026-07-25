15f6d5feea7d38da4997ee08dd5694e85e005aba
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 02:26:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Posts from "./pages/Posts";
-import Authors from "./pages/Authors";
-import Categories from "./pages/Categories";
-import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div
-      style={{
-        color: "white",
-        padding: "40px",
-        fontSize: "30px",
-      }}
-    >
-      FlashNews24 CMS Working
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   );
 }
 
