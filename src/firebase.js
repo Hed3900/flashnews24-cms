a6b060a5f0b68e7b84cfa91ebc762cbea443cec9
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 00:14:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD3iAhm2hzUabQf9TIfrSxa7o10tFCMmqA",
+  authDomain: "flashnews24-5bfd6.firebaseapp.com",
+  projectId: "flashnews24-5bfd6",
+  storageBucket: "flashnews24-5bfd6.firebasestorage.app",
+  messagingSenderId: "192814639105",
+  appId: "1:192814639105:web:2fd1fbe341d8c33e577800",
+  measurementId: "G-E20Q4FXVCM"
 };
 
 const app = initializeApp(firebaseConfig);
