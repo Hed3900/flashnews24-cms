2895fd9183ffa3a555d91af07401730c5e2c6d51
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 03:55:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Posts from "./pages/Posts";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/posts" element={<Posts />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/posts" element={<Posts />} />
+    </Routes>
   );
 }
 
