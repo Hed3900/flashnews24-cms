1d32e2a198398eb17f59425cc50ce934beb1d984
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 14:13:58 +0530

    Update NewPost.jsx

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -55,12 +55,12 @@ const handleImageUpload = async (e) => {
   formData.append("upload_preset", UPLOAD_PRESET);
 
   const res = await fetch(
-    `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
+  `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+  {
+    method: "POST",
+    body: formData,
+  }
+);
 
   const data = await res.json();
 
