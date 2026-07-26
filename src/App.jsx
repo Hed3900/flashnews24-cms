01b29fdaf57d70820fde8ed9c3d1ad98cbb36cbb
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 12:40:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import Posts from "./pages/Posts";
 import NewPost from "./pages/NewPost";
 import Categories from "./pages/Categories";
+import Authors from "./pages/Authors";
 function App() {
   return (
     <Routes>
@@ -10,6 +11,7 @@ function App() {
       <Route path="/posts" element={<Posts />} />
       <Route path="/new-post" element={<NewPost />} />
       <Route path="/categories" element={<Categories />} />
+      <Route path="/authors" element={<Authors />} />
     </Routes>
   );
 }
