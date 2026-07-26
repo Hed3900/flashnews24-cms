ded1cf14964325c5665efcbb792fd7afe5770b2c
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 05:55:51 +0530

    Create bloggerService.js

---
+++ src/services/bloggerService.js
@@ -0,0 +1,43 @@
+const API_KEY = "AIzaSyBX5-m0a9hbjZr3Ni31SayRN2tifGOMTNw";
+const CLIENT_ID = "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";
+const BLOG_ID = "4592212551421716018
+";
+
+const SCOPES = "https://www.googleapis.com/auth/blogger";
+
+export async function initGoogleAuth() {
+  return new Promise((resolve) => {
+    gapi.load("client:auth2", async () => {
+      await gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        scope: SCOPES,
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest",
+        ],
+      });
+      resolve();
+    });
+  });
+}
+
+export async function signIn() {
+  const auth = gapi.auth2.getAuthInstance();
+  if (!auth.isSignedIn.get()) {
+    await auth.signIn();
+  }
+}
+
+export async function publishPost(title, content, labels = []) {
+  await signIn();
+
+  return gapi.client.blogger.posts.insert({
+    blogId: BLOG_ID,
+    isDraft: false,
+    resource: {
+      title,
+      content,
+      labels,
+    },
+  });
+}
