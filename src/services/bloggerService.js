7b9d4a549b4f2dae31cd286bb00bfdf267a3594e
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-26 06:09:02 +0530

    Update bloggerService.js

--- src/services/bloggerService.js
+++ src/services/bloggerService.js
@@ -1,28 +1,41 @@
 const API_KEY = "AIzaSyBX5-m0a9hbjZr3Ni31SayRN2tifGOMTNw";
-const CLIENT_ID = "192814639105-ru733dedoeplvgf9qtpkkc2ltgj6lgg8.apps.googleusercontent.com";
-const BLOG_ID = "4592212551421716018
-";
+const CLIENT_ID =
+  "192814639105-ru733dedoeplvgf9qtpkkc2ltgj6lgg8.apps.googleusercontent.com";
+const BLOG_ID = "4592212551421716018";
 
 const SCOPES = "https://www.googleapis.com/auth/blogger";
 
+let initialized = false;
+
 export async function initGoogleAuth() {
-  return new Promise((resolve) => {
-    gapi.load("client:auth2", async () => {
-      await gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        scope: SCOPES,
-        discoveryDocs: [
-          "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest",
-        ],
-      });
-      resolve();
+  if (initialized) return;
+
+  return new Promise((resolve, reject) => {
+    window.gapi.load("client:auth2", async () => {
+      try {
+        await window.gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          scope: SCOPES,
+          discoveryDocs: [
+            "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest",
+          ],
+        });
+
+        initialized = true;
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
     });
   });
 }
 
 export async function signIn() {
-  const auth = gapi.auth2.getAuthInstance();
+  await initGoogleAuth();
+
+  const auth = window.gapi.auth2.getAuthInstance();
+
   if (!auth.isSignedIn.get()) {
     await auth.signIn();
   }
@@ -31,7 +44,7 @@ export async function signIn() {
 export async function publishPost(title, content, labels = []) {
   await signIn();
 
-  return gapi.client.blogger.posts.insert({
+  return window.gapi.client.blogger.posts.insert({
     blogId: BLOG_ID,
     isDraft: false,
     resource: {
