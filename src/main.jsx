488cf6860a0a3c7c97c805e061068811b3e25714
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-27 14:11:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import "react-quill/dist/quill.snow.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
