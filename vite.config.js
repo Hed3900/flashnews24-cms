e7912b3b83c5c3a32638671f1ab6d7bb824130f2
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-07-31 00:10:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/flashnews24-cms/",
-
+  base: "/",
   plugins: [react()],
 
   server: {
