31793a7dcb607ed05802a3c9a32e4c3aa3805479
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-08-01 11:57:26 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -11,9 +11,8 @@ exports.authorizeBlogger = onRequest(
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  "https://us-central1-flashnews24-5bfd6.cloudfunctions.net/oauthCallback"
+  process.env.REDIRECT_URI
 );
-
       const url = oauth2Client.generateAuthUrl({
         access_type: "offline",
         prompt: "consent",
