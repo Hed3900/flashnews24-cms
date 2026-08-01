9c23b43732451b4abae8afba1ce01f94fba8ee63
Hed3900 <97125456+Hed3900@users.noreply.github.com>
2026-08-01 12:19:02 +0530

    Refactor authorizeBlogger function for clarity

--- functions/index.js
+++ functions/index.js
@@ -8,46 +8,36 @@ admin.initializeApp();
 const CLIENT_ID = defineSecret("CLIENT_ID");
 const CLIENT_SECRET = defineSecret("CLIENT_SECRET");
 const REDIRECT_URI = defineSecret("REDIRECT_URI");
-const { onRequest } = require("firebase-functions/v2/https");
-const admin = require("firebase-admin");
-const { google } = require("googleapis");
-admin.initializeApp();
-exports.authorizeBlogger = onRequest(
-  { cors: true },
-  async (req, res) => {
-    try {
-      const { google } = require("googleapis");
+
 
 exports.authorizeBlogger = onRequest(
   {
     cors: true,
-    secrets: [CLIENT_ID, CLIENT_SECRET, REDIRECT_URI]
+    secrets: [CLIENT_ID, CLIENT_SECRET, REDIRECT_URI],
   },
   async (req, res) => {
-    const oauth2Client = new google.auth.OAuth2(
-      CLIENT_ID.value(),
-      CLIENT_SECRET.value(),
-      REDIRECT_URI.value()
-    );
-    // ...
-  }
-);
+    try {
+      const oauth2Client = new google.auth.OAuth2(
+        CLIENT_ID.value(),
+        CLIENT_SECRET.value(),
+        REDIRECT_URI.value()
+      );
+
       const url = oauth2Client.generateAuthUrl({
         access_type: "offline",
         prompt: "consent",
         scope: [
-          "https://www.googleapis.com/auth/blogger"
-        ]
+          "https://www.googleapis.com/auth/blogger",
+        ],
       });
 
       res.redirect(url);
-
     } catch (err) {
       console.error(err);
 
       res.status(500).json({
         success: false,
-        error: err.message
+        error: err.message,
       });
     }
   }
